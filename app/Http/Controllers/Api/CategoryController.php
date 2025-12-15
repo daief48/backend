7d799f45a56329a297be4791dfed3c0926cbf9f1
daief48 <daief20103048@gmail.com>
2025-12-15 22:06:08 +0600

    create routen and controller for category crud

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Category::create(
+            $request->validate(['name' => 'required|string'])
+        );
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $category->update(
+            $request->validate(['name' => 'required|string'])
+        );
+        return $category;
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->noContent();
+    }
+}
